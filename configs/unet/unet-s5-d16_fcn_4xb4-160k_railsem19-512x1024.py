5fe233a2f532ad4f6a80796165b314914aba5707
Arian Izadi <15961653+arianizadi@users.noreply.github.com>
2026-03-06 10:26:00 -0800

    Update to support 4070 ti super

--- configs/unet/unet-s5-d16_fcn_4xb4-160k_railsem19-512x1024.py
+++ configs/unet/unet-s5-d16_fcn_4xb4-160k_railsem19-512x1024.py
@@ -1,7 +1,11 @@
+import warnings
+warnings.filterwarnings("ignore", message="Detected call of `scheduler.step\(\)` before `optimizer.step\(\)`")
+
 _base_ = ['./unet-s5-d16_fcn_4xb4-160k_cityscapes-512x1024.py']
 dataset_type = 'RailSem19Dataset'
 data_root = 'data/RailSem19/'
 train_dataloader = dict(
+    batch_size=2,
     dataset=dict(
         type=dataset_type,
         data_root=data_root,
@@ -29,7 +33,24 @@ visualizer = dict(type='SegLocalVisualizer', vis_backends=vis_backends, name='vi
 load_from = 'https://download.openmmlab.com/mmsegmentation/v0.5/unet/fcn_unet_s5-d16_4x4_512x1024_160k_cityscapes/fcn_unet_s5-d16_4x4_512x1024_160k_cityscapes_20211210_145204-6860854e.pth'
 
 
-optimizer = dict(type='SGD', lr=0.00125, momentum=0.9, weight_decay=0.0005)
-optim_wrapper = dict(type='OptimWrapper', optimizer=optimizer, clip_grad=None)
+optimizer = dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0005)
+optim_wrapper = dict(
+    type='AmpOptimWrapper',
+    optimizer=optimizer,
+    accumulative_counts=8,
+    clip_grad=None)
 
-model = dict(backbone=dict(norm_eval=True))
+train_cfg = dict(type='IterBasedTrainLoop', max_iters=1280000, val_interval=128000)
+param_scheduler = [
+    dict(
+        type='PolyLR',
+        eta_min=1e-4,
+        power=0.9,
+        begin=0,
+        end=1280000,
+        by_epoch=False)
+]
+default_hooks = dict(
+    checkpoint=dict(type='CheckpointHook', by_epoch=False, interval=128000))
+
+model = dict(backbone=dict(with_cp=True, norm_eval=True))
